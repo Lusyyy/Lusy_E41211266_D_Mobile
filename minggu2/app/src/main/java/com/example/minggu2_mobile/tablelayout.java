79784af682ab4a6a67cc48e45a9b390939b9b253
Lusyyy <lusyyd1@gmail.com>
2023-03-08 08:10:37 +0700

    all

---
+++ minggu2/app/src/main/java/com/example/minggu2_mobile/tablelayout.java
@@ -0,0 +1,18 @@
+package com.example.minggu2_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class tablelayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tablelayout);
+    }
+
+    public void menu1(View view) {
+    }
+}
