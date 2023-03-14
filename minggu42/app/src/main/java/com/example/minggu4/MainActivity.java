8cebc3a1d7d74ccc2824c068abe17a55854b1ebe
Lusyyy <lusyyd1@gmail.com>
2023-03-15 00:25:50 +0700

    minggu 5

---
+++ minggu42/app/src/main/java/com/example/minggu4/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.minggu4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url=editText.getText().toString();
+                Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+    }
+}
